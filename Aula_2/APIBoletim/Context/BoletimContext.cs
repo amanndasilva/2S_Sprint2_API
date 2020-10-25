467fd6138f7d3562b8c128a1bb6dcc55ebbf38e4
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-10-25 16:25:32 -0300

    Exercício parcialmente resolvido - troca de computadores

---
+++ Aula_2/APIBoletim/Context/BoletimContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Context
+{
+    public class BoletimContext
+    {
+        SqlConnection con = new SqlConnection();
+
+        public BoletimContext()
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-D5U2PUB\SQLEXPRESS;Initial Catalog=boletim;User ID=sa;Password=sa132;";
+        }
+
+        public SqlConnection Conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        //void não possui retorno
+        public void Desconectar()
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+    }
+}
--- Aula_2/APIBoletim/Repositories/AlunoRepository.cs
+++ Aula_2/APIBoletim/Repositories/AlunoRepository.cs
@@ -1,14 +1,22 @@
-﻿using APIBoletim.Domains;
+﻿using APIBoletim.Context;
+using APIBoletim.Domains;
 using APIBoletim.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIBoletim.Repositories
 {
-    public class AlunoReporitory : IAluno
+    public class AlunoRepository : IAluno
     {
+        //Chama a classe de conexão do Banco
+        BoletimContext conexao = new BoletimContext();
+
+        //Chama o obj que irá receber e executar os comando do Banco
+        SqlCommand cmd = new SqlCommand();
+
         public Aluno Alterar(Aluno a)
         {
             throw new NotImplementedException();
@@ -26,7 +34,16 @@ namespace APIBoletim.Repositories
 
         public List<Aluno> ListarTodos()
         {
-            throw new NotImplementedException();
+            //Abrir a conexão
+            cmd.Connection = conexao.Conectar();
+
+            //Preparar consulta - query
+            cmd.CommandText = "SELECT * FROM Aluno";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            //Fechar a conexão
+            conexao.Desconectar();
         }
 
         public Aluno Remover(Aluno a)
