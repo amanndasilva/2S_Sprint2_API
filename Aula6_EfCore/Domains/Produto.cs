001b64f6eb4822383343d146082f1a4bb10427ea
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-11-06 00:33:28 -0200

    Nome das pastas modificados e exercício de EfCore ainda não finalizado

---
+++ Aula6_EfCore/Domains/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula6_EfCore.Domains
+{
+    /// <summary>
+    /// Define a classe Produto
+    /// </summary>
+    public class Produto
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
