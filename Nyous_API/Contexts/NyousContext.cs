f0c2f0a9ed51f0aba4ee3e504a0621111a0d9b0c
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-11-09 19:49:29 -0200

    Exercício da aula 12-JWT finalizado

---
+++ Nyous_API/Contexts/NyousContext.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Nyous_API.Domains;
+
+namespace Nyous_API.Contexts
+{
+    public partial class NyousContext : DbContext
+    {
+        public NyousContext()
+        {
+        }
+
+        public NyousContext(DbContextOptions<NyousContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Acesso> Acesso { get; set; }
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Convite> Convite { get; set; }
+        public virtual DbSet<Evento> Evento { get; set; }
+        public virtual DbSet<Localizacao> Localizacao { get; set; }
+        public virtual DbSet<Presenca> Presenca { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-D5U2PUB\\SQLEXPRESS;Initial Catalog=Nyous;Persist Security Info=True;User ID=sa;Password=sa132");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Acesso>(entity =>
+            {
+                entity.HasKey(e => e.IdAcesso)
+                    .HasName("PK__Acesso__CDF01DA182275138");
+
+                entity.Property(e => e.Tipo)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__Categori__A3C02A102B01602E");
+
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Convite>(entity =>
+            {
+                entity.HasKey(e => e.IdConvite)
+                    .HasName("PK__Convite__318FC554C331DC31");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Convite)
+                    .HasForeignKey(d => d.IdEvento)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Convite__IdEvent__47DBAE45");
+
+                entity.HasOne(d => d.IdUsuarioConvidadoNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioConvidadoNavigation)
+                    .HasForeignKey(d => d.IdUsuarioConvidado)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Convite__IdUsuar__49C3F6B7");
+
+                entity.HasOne(d => d.IdUsuarioEmissorNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioEmissorNavigation)
+                    .HasForeignKey(d => d.IdUsuarioEmissor)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Convite__IdUsuar__48CFD27E");
+            });
+
+            modelBuilder.Entity<Evento>(entity =>
+            {
+                entity.HasKey(e => e.IdEvento)
+                    .HasName("PK__Evento__034EFC040CC83318");
+
+                entity.Property(e => e.AcessoRestrito)
+                    .HasMaxLength(1)
+                    .IsFixedLength()
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.DataEvento).HasColumnType("datetime");
+
+                entity.Property(e => e.Decaricao)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdCategoriaNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Evento__IdCatego__403A8C7D");
+
+                entity.HasOne(d => d.IdLocalizacaoNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdLocalizacao)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Evento__IdLocali__412EB0B6");
+            });
+
+            modelBuilder.Entity<Localizacao>(entity =>
+            {
+                entity.HasKey(e => e.IdLocalizacao)
+                    .HasName("PK__Localiza__C96A5BF6BABB83D0");
+
+                entity.Property(e => e.Bairro)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cep)
+                    .IsRequired()
+                    .HasColumnName("CEP")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cidade)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Complemento)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Logradouro)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Numero)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Uf)
+                    .IsRequired()
+                    .HasColumnName("UF")
+                    .HasMaxLength(2)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Presenca>(entity =>
+            {
+                entity.HasKey(e => e.IdPresenca)
+                    .HasName("PK__Presenca__50FB6F5D71F7366C");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Presenca)
+                    .HasForeignKey(d => d.IdEvento)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Presenca__IdEven__44FF419A");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Presenca)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Presenca__IdUsua__440B1D61");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__5B65BF97A34F0712");
+
+                entity.Property(e => e.DataNascimento).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(80)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdAcessoNavigation)
+                    .WithMany(p => p.Usuario)
+                    .HasForeignKey(d => d.IdAcesso)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Usuario__IdAcess__3C69FB99");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Nyous_API/Controllers/LoginController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Nyous_API.Contexts;
+using Nyous_API.Domains;
+
+namespace Nyous_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        //Chama nosso contexto do banco
+        NyousContext _context = new NyousContext();
+
+        //Capturar as inf do token do appsettings.json
+        //Define uma variável para percorrer os métodos com as configurações obtidas no appsettings.json
+        private IConfiguration _config;
+
+        //Define um método construtor para poder passar essas configs
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private Usuario AuthenticateUser(Usuario login)
+        {
+            return _context.Usuario
+                .Include(a => a.IdAcessoNavigation)
+                .FirstOrDefault(u => u.Email == login.Email && u.Senha == login.Senha);
+        }
+
+        //Cria nosso método que vai gerar o Token
+        private string GenerateJSONWebToken(Usuario userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            //Cria a credencial com a security key criada, passando o alg
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            //Definimos nossas Claims (dados da sessão) para poderem ser capturadas a qualquer momento enquanto o Token for ativo
+            var claims = new[] {
+                //NameId, Email e Jti -> são padrões
+                new Claim(JwtRegisteredClaimNames.NameId, userInfo.Nome),
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                //Pega info de acesso por escrito, ex: Padrao ou Administrador
+                new Claim(ClaimTypes.Role, userInfo.IdAcessoNavigation.Tipo)
+            };
+
+            //Configura nosso Token e seu tempo de vida
+            var token = new JwtSecurityToken
+            (
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        //Usamos a anotação "AllowAnonymous" para ignorar a autenticação neste método, já que é ele quem fará isso
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] Usuario login)
+        {
+            //Definimos logo de cara como Unauthorized (não autorizado)
+            IActionResult response = Unauthorized();
+
+            //Autentica o usuário da API
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+    }
+}
